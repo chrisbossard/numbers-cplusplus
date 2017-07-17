a3f0ee304745baf41020c28f11c79394d5131134
chrisbossard <cdbossard@gmail.com>
2017-07-17 23:44:37 +0900

    Created numbers.cpp
    Added implementation file

---
+++ numbers.cpp
@@ -0,0 +1,120 @@
+// Implementation of Class numbers
+#include "numbers.h"
+using namespace numbers;
+
+// destructor
+numbers::~numbers() {
+    clearbuf();
+}
+
+// assignment operator
+uint64_t numbers::operator = ( const uint64_t num ) {
+    setnum(num);
+    return getnum();
+}
+
+const char * numbers::words() {
+    return words(_num);
+}
+
+// convert to words
+const char * numbers::words( const uint64_t num ) {
+    if (num > _MAXNUM) {
+        return errnum;
+    }
+    initbuf();
+    uint64_t n = num;
+    if (n == 0) {
+        appendbuf(_singles[n]);
+        return _buf;
+    }
+
+    // powers of 1000
+    if (n >= 1000) {
+        for(int i = 5; i > 0; --i) {
+            uint64_t power = (uint64_t) pow(1000.0, i);
+            uint64_t _n = ( n - ( n % power ) ) / power;
+            if (_n) {
+                int index = i;
+                numbers _nw(_n);
+                appendspace();
+                appendbuf(_nw.words());
+                appendspace();
+                appendbuf(_powers[index]);
+                n -= _n * power;
+            }
+        }
+    }
+    // hundreds
+    if (n >= 100 && n < 1000) {
+        uint64_t _n = ( n - ( n % 100 ) ) / 100;
+        numbers _nw(_n);
+        appendspace();
+        appendbuf(_nw.words());
+        appendspace();
+        appendbuf(_hundred);
+        n -= _n * 100;
+    }
+    // tens
+    if (n >= 20 && n < 100) {
+        uint64_t _n = ( n - ( n % 10 ) ) / 10;
+        appendspace();
+        appendbuf(_tens[_n]);
+        n -= _n * 10;
+        hyphen_flag = true;
+    }
+    // teens
+    if (n >= 10 && n < 20) {
+        appendspace();
+        appendbuf(_teens[n - 10]);
+        n = 0;
+    }
+    // singles
+    if (n > 0 && n < 10) {
+        appendspace();
+        appendbuf(_singles[n]);
+    }
+    return _buf;
+}
+
+// -- private methods --
+
+// reset the buffer
+void numbers::clearbuf() {
+    if (_buf != nullptr) {
+        free(_buf);
+        _buf = nullptr;
+    }
+    _buflen = 0;
+}
+
+// initialize the buffer
+void numbers::initbuf() {
+    clearbuf();
+    _buf = (char *) malloc(_NUMWORD_MAXSTR);
+    *_buf = 0;
+    hyphen_flag = false;
+}
+
+// append space (or hyphen)
+void numbers::appendspace() {
+    if (_buflen) {
+        appendbuf( hyphen_flag ? _hyphen : _space);
+        hyphen_flag = false;
+    }
+}
+
+// append text to the string buffer
+void numbers::appendbuf(const char * s) {
+    if(!s) return;
+    size_t slen = strnlen(s, _NUMWORD_MAXSTR);
+    if (slen < 1) {
+        return;
+    }
+    if ((slen + _buflen + 1) >= _NUMWORD_MAXSTR) {
+        return;
+    }
+    memcpy(_buf + _buflen, s, slen);
+    _buflen += slen;
+    _buf[_buflen] = 0;
+}
